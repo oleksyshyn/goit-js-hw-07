c70b874947a665d5a1212b080c50a69c47a58379
Iryna Oleksyshyn <irynkaoleksyshyn@gmail.com>
2023-03-29 00:36:36 +0300

    Add task 2

--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,36 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const galleryContainer = document.querySelector('.gallery');
+const galleryMarkup = createImageGalleryMarkup(galleryItems);
+
+function createImageGalleryMarkup(gallery) {
+    return gallery
+        .map(({ preview, original, description }) => {
+            return `
+        <li class="gallery__item">
+            <a class="gallery__link" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}" />
+            </a>
+        </li>
+        `;
+        })
+        .join('');
+}
+galleryContainer.innerHTML = galleryMarkup;
+galleryContainer.addEventListener('click', onSelectedGalleryCard)
+
+function onSelectedGalleryCard(event) {
+    event.preventDefault();
+    
+    if (event.target.nodeName !== 'IMG') {
+        return;
+    }
+
+    const gallary = new SimpleLightbox('.gallery a', {
+        captionsData: 'alt',
+        captionDelay: 250,
+    });
+    
+}
+
